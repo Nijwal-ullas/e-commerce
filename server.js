bd043c6b373a1740620dadd62f8c3e5fdcbd040c
nijwal <nijwal12@gmail.com>
2025-10-23 10:02:50 +0530

    user home ,register,signup pages

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const env = require('dotenv');
-const path = require('path');
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-env.config();
-const port = process.env.PORT;;
 
+const port = process.env.PORT;
 
-app.get('/',(req,res)=>{
-    res.render('home');
+app.get('/user/home',(req,res)=>{
+    console.log("hallo")
+    res.render('user/home');
 })
-
-
-
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
