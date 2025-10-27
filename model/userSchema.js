8235d5f24e62a99434778c20fbdcea2808ca52e2
nijwal <nijwal12@gmail.com>
2025-10-27 10:30:20 +0530

    userSchema

--- model/userSchema.js
+++ model/userSchema.js
@@ -4,12 +4,12 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   Name: { type: String, required: true, trim: true },
-  Password: { type: String },
+  Password: { type: String , required: false },
   IsBlocked: { type: Boolean, default: false },
-  Phone: { type: String },
+  Phone: { type: String , required: false, unique: true, sparse: true,default: null},
   ProfileImage: { type: String },
   Email: { type: String, required: true, unique: true, lowercase: true },
-  googleId: { type: String },
+  googleId: { type: String,unique: true, sparse: true},
   UpdatedAt: { type: Date },
   CreatedAt: { type: Date },
 });
