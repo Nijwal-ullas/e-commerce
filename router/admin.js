ea62cc07551532257667f15d3241d21af009a523
nijwal <nijwal12@gmail.com>
2025-10-29 19:06:38 +0530

    adminRouter

--- router/admin.js
+++ router/admin.js
@@ -2,10 +2,12 @@ import express from 'express';
 const router = express.Router();
 import adminController from '../controller/adminController.js';
 import auth from '../middleware/auth.js';
+import customerController from '../controller/customerController.js';
 
 router.get('/login', adminController.loadAdminLoginPage)
 router.post('/login',adminController.login)
 router.get('/dashboard',auth.adminAuth,adminController.loadDashboardPage)
 router.get('/logout',adminController.logout)
+router.get('/users',auth.adminAuth,customerController.customerInfo)
 
 export default router; 
