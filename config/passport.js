f21ac85134beeba26ba274c4df119b4f0d8fdd05
nijwal <nijwal12@gmail.com>
2025-10-30 21:47:07 +0530

    passport

--- config/passport.js
+++ config/passport.js
@@ -10,17 +10,18 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/auth/google/callback",
+      callbackURL: "http://localhost:3040/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
         let user = await User.findOne({ googleId: profile.id });
+
         if (user) {
           return done(null, user);
         } else {
           user = new User({
             name: profile.displayName,
-            email: profile.emails?.[0]?.value || 'noemail@example.com',
+            email: profile.emails?.[0]?.value || "noemail@example.com",
             googleId: profile.id,
           });
           await user.save();
