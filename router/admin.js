62a072c643e1f85b4aee02a9e11341b5226926fa
nijwal <nijwal12@gmail.com>
2025-10-30 21:46:23 +0530

    adminRouter

--- router/admin.js
+++ router/admin.js
@@ -1,13 +1,15 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import adminController from '../controller/adminController.js';
-import auth from '../middleware/auth.js';
-import customerController from '../controller/customerController.js';
+import adminController from "../controller/adminController.js";
+import auth from "../middleware/auth.js";
+import customerController from "../controller/customerController.js";
 
-router.get('/login', adminController.loadAdminLoginPage)
-router.post('/login',adminController.login)
-router.get('/dashboard',auth.adminAuth,adminController.loadDashboardPage)
-router.get('/logout',adminController.logout)
-router.get('/users',auth.adminAuth,customerController.customerInfo)
+router.get("/login", adminController.loadAdminLoginPage);
+router.post("/login", adminController.login);
+router.get("/dashboard", auth.adminAuth, adminController.loadDashboardPage);
+router.get("/logout", adminController.logout);
+router.get("/users", auth.adminAuth, customerController.customerInfo);
+router.get('/blockCustomer',auth.adminAuth,customerController.blockCustomer)
+router.get('/unblockCustomer',auth.adminAuth,customerController.unblockCustomer)
 
-export default router; 
+export default router;
