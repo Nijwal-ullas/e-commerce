8601cd466f60f6c0f18c0c495f711cf541b0994f
nijwal <nijwal12@gmail.com>
2025-10-30 21:46:09 +0530

    customerController

--- controller/customerController.js
+++ controller/customerController.js
@@ -1,38 +1,58 @@
-import User from '../model/userSchema.js';
+import user from "../model/userSchema.js";
 
 const customerInfo = async (req, res) => {
-    try {
-        let search = req.query.search || '';
-        let page = parseInt(req.query.page) || 1;
-        const limit = 3;
-
-        const userData = await User.find({
-            $or: [
-                { Name: { $regex: '.*' + search + '.*', $options: 'i' } },
-                { Email: { $regex: '.*' + search + '.*', $options: 'i' } }
-            ]
-        })
-            .limit(limit)
-            .skip((page - 1) * limit)
-            .exec();
-
-        const count = await User.countDocuments({
-            $or: [
-                { Name: { $regex: '.*' + search + '.*', $options: 'i' } },
-                { Email: { $regex: '.*' + search + '.*', $options: 'i' } }
-            ]
-        });
-
-        res.render('admin/customers', {
-            users: userData,
-            totalUsers: count,
-            currentPage: page,
-            totalPages: Math.ceil(count / limit)
-        });
-    } catch (error) {
-        console.error('Error loading customers:', error);
-        res.status(500).send('Server Error');
-    }
+  try {
+    let search = req.query.search || "";
+    let page = parseInt(req.query.page) || 1;
+    const limit = 3;
+    const users = await user
+      .find({
+        $or: [
+          { name: { $regex: ".*" + search + ".*", $options: "i" } },
+          { email: { $regex: ".*" + search + ".*", $options: "i" } },
+        ],
+      })
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec();
+    const count = await user.countDocuments({
+      $or: [
+        { name: { $regex: ".*" + search + ".*", $options: "i" } },
+        { email: { $regex: ".*" + search + ".*", $options: "i" } },
+      ],
+    });
+    res.render("admin/customers", {
+      users,
+      totalUsers: count,
+      currentPage: page,
+      totalPages: Math.ceil(count / limit),
+    });
+  } catch (error) {
+    console.error("Error loading customers:", error);
+    res.status(500).send("Server Error");
+  }
 };
 
-export default { customerInfo };
+const blockCustomer = async (req, res) => {
+  try {
+    const userId = req.query.id;
+    await user.updateOne({ _id: userId }, { $set: { isBlocked: true } });
+    res.redirect("/admin/users");
+  } catch (error) {
+    console.error("Error loading customers:", error);
+    res.status(500).send("Server Error");
+  }
+};
+
+const unblockCustomer = async (req, res) => {
+  try {
+    const userId = req.query.id;
+    await user.updateOne({ _id: userId }, { $set: { isBlocked: false } });
+    res.redirect("/admin/users");
+  } catch (error) {
+    console.error("Error loading customers:", error);
+    res.status(500).send("Server Error");
+  }
+};
+
+export default { customerInfo, blockCustomer, unblockCustomer };
