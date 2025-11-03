470e5a2cc3eee3c45960911027bbc39b2772f585
nijwal <nijwal12@gmail.com>
2025-11-03 10:42:45 +0530

    catagorySchema

--- model/catagorySchema.js
+++ model/catagorySchema.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const { Schema, ObjectId } = mongoose;
 
 const CatagoriesSchema = new Schema({
-  Name: { type: String },
-  UpdatedAt: { type: Date },
-  CreatedAt: { type: Date },
-  Description: { type: String },
-  IsListed: { type: Boolean },
+  name: { type: String },
+  updatedAt: { type: Date },
+  createdAt: { type: Date },
+  description: { type: String },
+  isListed: { type: Boolean, default: true},
 });
 
 const Catagories = mongoose.model('Catagories', CatagoriesSchema);
