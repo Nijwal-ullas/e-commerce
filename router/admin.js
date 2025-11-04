a628f565123efea8b3f0b24afb9d144d79716d80
nijwal <nijwal12@gmail.com>
2025-11-04 09:04:23 +0530

    adminRouter

--- router/admin.js
+++ router/admin.js
@@ -3,13 +3,34 @@ const router = express.Router();
 import adminController from "../controller/adminController.js";
 import auth from "../middleware/auth.js";
 import customerController from "../controller/customerController.js";
+import catagoryController from "../controller/catagoryController.js";
+import brandController from "../controller/brandController.js";
+import multer from "multer";
+import storage from "../helpers/multer.js";
+
+const upload = multer({ storage: storage });
 
 router.get("/login", adminController.loadAdminLoginPage);
 router.post("/login", adminController.login);
 router.get("/dashboard", auth.adminAuth, adminController.loadDashboardPage);
 router.get("/logout", adminController.logout);
+
+// customer routes
 router.get("/users", auth.adminAuth, customerController.customerInfo);
 router.get('/blockCustomer',auth.adminAuth,customerController.blockCustomer)
 router.get('/unblockCustomer',auth.adminAuth,customerController.unblockCustomer)
+router.delete('/deleteUser/:id',auth.adminAuth,customerController.deleteUser)
+
+// Category routes
+router.get("/catagory", auth.adminAuth, catagoryController.catagoryPage);
+router.post('/addCatagory',auth.adminAuth,catagoryController.addCatagory)
+router.put("/editCatagory/:id",auth.adminAuth, catagoryController.editCatagory);
+router.delete("/deleteCatagory/:id", auth.adminAuth,catagoryController.deleteCatagory);
+router.patch("/listCategory/:id",auth.adminAuth,catagoryController.listCatagory);
+router.patch("/unlistCategory/:id",auth.adminAuth,catagoryController.unlistCatagory);
+
+//Brand routes
+router.get("/brand", auth.adminAuth, brandController.brandPage);
+
 
 export default router;
