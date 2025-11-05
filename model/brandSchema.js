62cbe09d8d56247f03aa15ca12e41802b13ad401
nijwal <nijwal12@gmail.com>
2025-11-05 15:56:35 +0530

    brandSchema

--- model/brandSchema.js
+++ model/brandSchema.js
@@ -4,9 +4,11 @@ const { Schema, ObjectId } = mongoose;
 
 const BrandSchema = new Schema({
   name: { type: String },
-  createdAt: { type: Date },
+  description: { type: String },
   brandLogo: { type: String },
-});
+},
+{ timestamps: true }   
+);
 
 const Brand = mongoose.model('Brand', BrandSchema);
 
