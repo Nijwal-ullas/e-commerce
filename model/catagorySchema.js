24c63617a9667e2db30af0d176fa8ad6fbf604a0
nijwal <nijwal12@gmail.com>
2025-11-05 17:34:04 +0530

    catagorySchema

--- model/catagorySchema.js
+++ model/catagorySchema.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 
 const { Schema, ObjectId } = mongoose;
 
-const CatagoriesSchema = new Schema({
-  name: { type: String },
-  updatedAt: { type: Date },
-  createdAt: { type: Date },
-  description: { type: String },
-  isListed: { type: Boolean, default: true},
-});
+const CatagoriesSchema = new Schema(
+  {
+    name: { type: String },
+    description: { type: String },
+    isListed: { type: Boolean, default: true },
+  },
+  { timestamps: true }   
+);
 
-const Catagories = mongoose.model('Catagories', CatagoriesSchema);
+const Catagories = mongoose.model("Catagories", CatagoriesSchema);
 
 export default Catagories;
