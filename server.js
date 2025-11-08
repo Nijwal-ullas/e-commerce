3cede33044e8ad001f695ec86a986c6dff73dd01
nijwal <nijwal12@gmail.com>
2025-11-08 09:07:42 +0530

    server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 import userRouter from "./router/user/user.js";
 import adminRouter from "./router/admin/admin.js";
+import auth from "./middleware/auth.js";
 import passport from "./config/passport.js";
 import morgan from "morgan";
 
@@ -26,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
+    name : "admin-session",
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
@@ -41,18 +43,29 @@ app.use(
   })
 );
 
+app.use(
+  session({
+    name : "user-session",
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: "sessions",
+      ttl: 14 * 24 * 60 * 60,
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      secure: false,
+    },
+  })
+);
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req, res, next) => {
-  res.locals.user = req.session.user || req.user || null;
-  next();
-});
-
-app.use((req, res, next) => {
-  res.set("Cache-Control", "no-store");
-  next();
-});
+app.use(auth.setUser);
 
 app.use("/", userRouter);
 app.use("/admin", adminRouter);
