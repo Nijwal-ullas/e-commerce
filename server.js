85b8f6d9b8ff8a00437e72ffa2786e4f01450e9f
nijwal <nijwal12@gmail.com>
2025-11-11 22:00:34 +0530

    server

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.set("view engine", "ejs");
 app.use(morgan("dev"));
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
