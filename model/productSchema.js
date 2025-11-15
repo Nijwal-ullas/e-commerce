55a2c1da439dc59a42c757aceb5fe81d50728b7d
nijwal <nijwal12@gmail.com>
2025-11-16 00:18:45 +0530

    productSchema

--- model/productSchema.js
+++ model/productSchema.js
@@ -4,23 +4,20 @@ const ProductSchema = new mongoose.Schema(
   {
     productName: { type: String, required: true },
     description: { type: String },
-
     price: { type: Number, required: true },
     oldPrice: { type: Number },
     discount: { type: Number, default: 0 },
-
     category: { type: mongoose.Schema.Types.ObjectId, ref: "Categories" },
     brand: { type: mongoose.Schema.Types.ObjectId, ref: "Brand" },
-
     images: [{ type: String }],
-
-    stock: { type: Number, required: true },
-
-    isListed: { type: Boolean, default: true }
+    isListed: { type: Boolean, default: true },
+    VariantItem: [{
+      Ml: { type: Number, required: true },
+      Quantity: { type: Number, required: true, min: 0 },
+    }],
   },
   { timestamps: true }
 );
 
 const Product = mongoose.model("Product", ProductSchema);
-
-export default Product
+export default Product;
