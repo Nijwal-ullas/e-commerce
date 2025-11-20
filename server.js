f59fb28e9ec2ff782e76e1637341812ec6f86e6a
nijwal <nijwal12@gmail.com>
2025-11-20 22:37:09 +0530

    service

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ import session from "express-session";
 import MongoStore from "connect-mongo";
 import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
-import userRouter from "./router/user/user.js";
-import userProductRouter from "./router/user/productRouter.js"
+import userRouter from "./router/user/userRouter.js";
+import userProductRouter from "./router/user/productRouter.js";
 
-import adminRouter from "./router/admin/admin.js";
-import brandRouter from "./router/admin/brandRoutes.js"
-import categoryRouter from "./router/admin/categoryRoutes.js"
-import customerRouter from "./router/admin/customerRoutes.js"
-import productRouter from "./router/admin/productRoutes.js"
+import adminRouter from "./router/admin/adminRouter.js";
+import brandRouter from "./router/admin/brandRoutes.js";
+import categoryRouter from "./router/admin/categoryRoutes.js";
+import customerRouter from "./router/admin/customerRoutes.js";
+import productRouter from "./router/admin/productRoutes.js";
 
 import auth from "./middleware/auth.js";
 import passport from "./config/passport.js";
@@ -35,59 +35,63 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '0');
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
   next();
 });
 
-app.use(session({
-  name: "user-session",
-  secret: process.env.SESSION_SECRET + "-user",
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URI,
-    collectionName: "user-sessions",
-  }),
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24,
-    secure: false,
-    httpOnly: true
-  }
-}));
+app.use(
+  session({
+    name: "user-session",
+    secret: process.env.SESSION_SECRET + "-user",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: "user-sessions",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      secure: false,
+      httpOnly: true,
+    },
+  })
+);
 
-app.use("/admin", session({
-  name: "admin-session",
-  secret: process.env.SESSION_SECRET + "-admin",
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URI,
-    collectionName: "admin-sessions",
-  }),
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24,
-    secure: false,
-    httpOnly: true,
-    path: "/admin"
-  }
-}));
+app.use(
+  "/admin",
+  session({
+    name: "admin-session",
+    secret: process.env.SESSION_SECRET + "-admin",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: "admin-sessions",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      secure: false,
+      httpOnly: true,
+      path: "/admin",
+    },
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(auth.setUser);
 
-
 app.use("/", userRouter);
-app.use("/",userProductRouter);
+app.use("/", userProductRouter);
 
-app.use("/admin", adminRouter); 
-app.use("/admin",brandRouter);
-app.use("/admin",categoryRouter);
-app.use("/admin",customerRouter);
-app.use("/admin",productRouter);
+app.use("/admin", adminRouter);
+app.use("/admin", brandRouter);
+app.use("/admin", categoryRouter);
+app.use("/admin", customerRouter);
+app.use("/admin", productRouter);
 
 app.use((req, res) => {
   res.status(404).render("error");
@@ -96,7 +100,6 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).render("error.ejs");
 });
-
 
 const port = process.env.PORT;
 app.listen(port, () => {
