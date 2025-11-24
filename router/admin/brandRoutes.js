89e1151b1e1a9d3f11cca35d5a3cfd3d5ef280a2
nijwal <nijwal12@gmail.com>
2025-11-24 13:44:52 +0530

    brandRoter

--- router/admin/brandRoutes.js
+++ router/admin/brandRoutes.js
@@ -8,7 +8,7 @@ import upload from "../../helpers/multer.js";
 
 router.get('/brand', auth.adminAuth, brandController.brandPage);
 router.post('/brand', auth.adminAuth, upload.single('image'), brandController.addBrand);
-router.put('/brand/:id', auth.adminAuth, upload.single('image'), brandController.editBrand);
+router.put('/brand/:id', auth.adminAuth, upload.single('image'), upload.errorHandler, brandController.editBrand);
 router.delete('/brand/:id', auth.adminAuth, brandController.deleteBrand);
 
 
