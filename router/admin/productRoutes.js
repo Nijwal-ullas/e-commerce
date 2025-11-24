8ef42f38bcaca3741fa6c54ed7b7acd6419db762
nijwal <nijwal12@gmail.com>
2025-11-24 13:46:07 +0530

    adminProductController

--- router/admin/productRoutes.js
+++ router/admin/productRoutes.js
@@ -7,9 +7,9 @@ import upload from "../../helpers/multer.js";
 
 
 router.get('/product', auth.adminAuth, productController.productPage);
-router.post('/Product', auth.adminAuth, upload.array('images', 5), productController.addProduct);
+router.post('/product', auth.adminAuth, upload.array('images', 10), upload.errorHandler, productController.addProduct);
 router.get('/product/:id', auth.adminAuth, productController.getProduct);
-router.put('/Product/:id', auth.adminAuth, upload.array('images', 5), productController.editProduct);
+router.put('/product/:id', auth.adminAuth, upload.array('images', 10), upload.errorHandler, productController.editProduct)
 router.delete('/Product/:id', auth.adminAuth, productController.deleteProduct);
 router.get('/productsJSON', auth.adminAuth, productController.getProduct);
 
