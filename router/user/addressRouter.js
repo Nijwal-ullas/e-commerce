3ee2de14770e06aa92ecb5fc140f63f09fcb5634
nijwal <nijwal12@gmail.com>
2025-11-24 13:46:19 +0530

    addresRouter

---
+++ router/user/addressRouter.js
@@ -0,0 +1,15 @@
+import express from "express";
+import addressController from "../../controller/user/addressController.js"
+import auth from "../../middleware/auth.js"
+
+const router = express.Router();
+router.use(auth.checkUser)
+
+router.get("/address", addressController.loadAddressPage);
+router.get("/address/add", addressController.loadAddAddress); 
+router.post("/address/add", addressController.registerAddress); 
+router.get("/address/edit/:id", addressController.loadAddAddress); 
+router.post("/address/edit/:id", addressController.registerAddress); 
+router.delete("/address/delete/:id",addressController.deleteAddress)
+
+export default router;
