b632b9bbcc4bbdeb0717a552830eba7c1a29e396
nijwal <nijwal12@gmail.com>
2025-11-24 21:40:41 +0530

    profileRouter

--- router/user/profileRouter.js
+++ router/user/profileRouter.js
@@ -9,14 +9,16 @@ router.use(auth.checkUser)
     
 
 router.get("/profile",profileController.profilePage)
-router.post("/profile", upload.single('profileImage'), profileController.updateProfile); 
+router.get("/profile/edit", profileController.loadEditProfile);
+router.post("/profile/edit",upload.single("profileImage"),profileController.updateProfile); 
 router.get("/profile/change-email",profileController.changeEmail)
 router.post("/profile/change-email",profileController.verifyChangeEmail)
 router.get("/changeEmail-otp",profileController.loadOtpPage)
 router.post("/changeEmail-otp",profileController.registerOtpPage)
 router.post("/resend-otp",profileController.resendOtp)
 router.get("/profile/change-password",profileController.loadchangePassword);
-router.post("/profile/change-password",profileController.registerChangePassword)
+router.post("/profile/change-password",profileController.registerChangePassword);
+
 
 
 export default router;
