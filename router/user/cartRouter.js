e8ed0d182399f96005d8c88e4452d404a8725da9
nijwal <nijwal12@gmail.com>
2025-11-27 23:17:47 +0530

    cartRouter

---
+++ router/user/cartRouter.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cartController from "../../controller/user/cartController.js"
+import auth from "../../middleware/auth.js"
+
+const router = express.Router();
+router.use(auth.checkUser)
+
+
+router.get("/cart", cartController.getCart);
+router.post("/cart/update-quantity", cartController.updateQuantity); 
+router.post("/cart/remove/:id", cartController.removeFromCart);
+router.post("/cart/:id", cartController.addCart);
+
+export default router;
