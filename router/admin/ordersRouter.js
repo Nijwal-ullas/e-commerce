9732f73c3cb8d69132d6f6c3eade94377665b39b
nijwal <nijwal12@gmail.com>
2025-12-11 23:56:45 +0530

    adminOrdersRouter

--- router/admin/ordersRouter.js
+++ router/admin/ordersRouter.js
@@ -9,8 +9,8 @@ router.get("/adminOrders/:id", auth.adminAuth, orderController.getDetailPage);
 router.put("/adminOrders/:id/status", auth.adminAuth, orderController.updateOrderStatus);
 router.put("/adminOrders/:orderId/item/:itemId/status", auth.adminAuth, orderController.updateItemStatus);
 
-router.put("/adminOrders/:orderId/approve-return", auth.adminAuth, orderController.approveReturnRequest);
-router.put("/adminOrders/:orderId/reject-return", auth.adminAuth, orderController.rejectReturnRequest);
-router.put("/adminOrders/:orderId/process-refund", auth.adminAuth, orderController.processRefund);
+router.put("/adminOrders/:orderId/item/:itemId/approve-return", auth.adminAuth, orderController.approveItemReturn);
+router.put("/adminOrders/:orderId/item/:itemId/reject-return", auth.adminAuth, orderController.rejectItemReturn);
+router.put("/adminOrders/:orderId/item/:itemId/refund", auth.adminAuth, orderController.refundItem);
 
 export default router;
