4e4d9656bbc9e622e37f49e86734827c0a391a42
nijwal <nijwal12@gmail.com>
2025-12-14 22:17:44 +0530

    server

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ import brandRouter from "./router/admin/brandRoutes.js";
 import categoryRouter from "./router/admin/categoryRoutes.js";
 import customerRouter from "./router/admin/customerRoutes.js";
 import productRouter from "./router/admin/productRoutes.js";
-import ordersRouter from "./router/admin/ordersRouter.js"
+import ordersRouter from "./router/admin/ordersRouter.js";
+import couponRouter from "./router/admin/couponRouter.js";
 
 import auth from "./middleware/auth.js";
 import passport from "./config/passport.js";
@@ -110,7 +111,8 @@ app.use("/admin", brandRouter);
 app.use("/admin", categoryRouter);
 app.use("/admin", customerRouter);
 app.use("/admin", productRouter);
-app.use("/admin", ordersRouter)
+app.use("/admin", ordersRouter);
+app.use("/admin",couponRouter);
 
 app.use((req, res) => {
   res.status(404).render("error");
