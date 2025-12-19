028c271424c81008dc551e63949142e5ed997685
nijwal <nijwal12@gmail.com>
2025-12-19 10:49:09 +0530

    adminRouter

--- router/admin/adminRouter.js
+++ router/admin/adminRouter.js
@@ -9,7 +9,10 @@ router.get("/login", adminController.loadAdminLoginPage);
 router.post("/login", adminController.login);
 router.get("/dashboard", auth.adminAuth, adminController.loadDashboardPage);
 router.get("/logout", auth.adminAuth, adminController.logout);
-
+router.get("/order-status-report", auth.adminAuth,adminController.getOrderStatusReport);
+router.get("/sales-report",auth.adminAuth,adminController.getSalesReport);
+router.get("/sales-report/excel", auth.adminAuth,adminController.downloadExcel);
+router.get("/sales-report/pdf", auth.adminAuth,adminController.downloadPdf);
 
 
 export default router;
