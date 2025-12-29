01eac7fbf2eb512117acf1a36db2a3b4cc740d92
nijwal <nijwal12@gmail.com>
2025-12-29 22:46:02 +0530

    deleted files

--- router/user/couponRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import couponController from "../../controller/user/couponController.js";
-
-const router = express.Router();
-
-router.get("/coupon/available-coupon",couponController.getAvailableCoupon);
-
-
-export default router
