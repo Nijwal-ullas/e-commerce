78ef0a14c2fd2ddc79a6831e935f20f193e42573
nijwal <nijwal12@gmail.com>
2025-12-29 12:01:57 +0530

    adminSide routes

--- router/admin/ordersRouter.js
+++ router/admin/ordersRouter.js
@@ -5,10 +5,7 @@ import auth from "../../middleware/auth.js";
 
 router.get("/adminOrders", auth.adminAuth, orderController.getOrdersPage);
 router.get("/adminOrders/:id", auth.adminAuth, orderController.getDetailPage);
-
-router.put("/adminOrders/:id/status", auth.adminAuth, orderController.updateOrderStatus);
 router.put("/adminOrders/:orderId/item/:itemId/status", auth.adminAuth, orderController.updateItemStatus);
-
 router.put("/adminOrders/:orderId/item/:itemId/approve-return", auth.adminAuth, orderController.approveItemReturn);
 router.put("/adminOrders/:orderId/item/:itemId/reject-return", auth.adminAuth, orderController.rejectItemReturn);
 router.put("/adminOrders/:orderId/item/:itemId/refund", auth.adminAuth, orderController.refundItem);
