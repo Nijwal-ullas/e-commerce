b230cba0430f515d53562da2b1dad434285eef1b
nijwal <nijwal12@gmail.com>
2025-12-29 22:46:19 +0530

    admins controllers

--- controller/admin/couponController.js
+++ controller/admin/couponController.js
@@ -6,7 +6,7 @@ const getCoupons = async (req,res)=>{
     try {
 
         const page = parseInt(req.query.page) || 1;
-        const limit = 5;
+        const limit = 10;
         const skip = (page-1)*limit;
         const totalCoupons = await Coupons.countDocuments();
 
