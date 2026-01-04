7f1041796c53b5381b03b65d08665e6e357e01d3
nijwal <nijwal12@gmail.com>
2026-01-04 20:01:12 +0530

    helper

--- helpers/cloudinaryUpload.js
+++ helpers/cloudinaryUpload.js
@@ -1,14 +1,5 @@
-import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv';
+import cloudinary from "../helpers/cloudinary.js";
 
-dotenv.config();
-
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 export const uploadToCloudinary = (fileBuffer, folder = 'brands') => {
   return new Promise((resolve, reject) => {
