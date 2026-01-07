7a7f2505d8a497204bb36cd115020e3030c5a9c7
nijwal <nijwal12@gmail.com>
2026-01-07 09:59:52 +0530

    errormessage and statusCode

---
+++ utilities/errorMessages.js
@@ -0,0 +1,8 @@
+const errors = {
+  USER_NOT_FOUND: "User not found",
+  INVALID_REQUEST: "Invalid request",
+  UNAUTHORIZED: "Unauthorized access",
+  SERVER_ERROR: "Something went wrong",
+};
+
+export default errors;
