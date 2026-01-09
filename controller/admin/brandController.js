d1a07aea9de9cf84693aaf6d407c8f92aefa19e9
nijwal <nijwal12@gmail.com>
2026-01-09 18:19:55 +0530

    userSide controller

--- controller/admin/brandController.js
+++ controller/admin/brandController.js
@@ -58,10 +58,10 @@ const addBrand = async (req, res) => {
       });
     }
 
-    if (description && description.length > 100) {
+    if (description && description.length > 200) {
       return res.status(statusCode.BAD_REQUEST).json({
         success: false,
-        message: "Description cannot exceed 100 characters",
+        message: "Description cannot exceed 200 characters",
       });
     }
 
@@ -156,10 +156,10 @@ const editBrand = async (req, res) => {
       });
     }
 
-    if (description && description.length > 100) {
+    if (description && description.length > 200) {
       return res.status(statusCode.BAD_REQUEST).json({
         success: false,
-        message: "Description cannot exceed 100 characters",
+        message: "Description cannot exceed 200 characters",
       });
     }
 
