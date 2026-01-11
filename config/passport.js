9013ee78bcbe621004671cdc20274188c13bde3a
nijwal <nijwal12@gmail.com>
2026-01-11 15:34:10 +0530

    passport

--- config/passport.js
+++ config/passport.js
@@ -1,16 +1,16 @@
+import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../model/userSchema.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-import passport from "passport";
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: "https://ruhecollecton.info/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
