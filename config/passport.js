1b6e05d34c31d7503410c1a59132005d75c1cd2c
nijwal <nijwal12@gmail.com>
2026-01-11 19:04:09 +0530

    passport

--- config/passport.js
+++ config/passport.js
@@ -10,8 +10,11 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL
+      callbackURL: process.env.GOOGLE_CALLBACK_URL||"https://ruhecollecton.info/auth/google/callback",
+               
+
     },
+
     async (accessToken, refreshToken, profile, done) => {
       try {
         let email = profile.emails?.[0]?.value;
